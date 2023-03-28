edf39cc85ef38b47085c27a3be85366a46237b63
rvaquerizo4 <rvaqueri@jaumebalmes.net>
2023-03-28 18:20:37 +0200

    examen

---
+++ EXAMEN/index.js
@@ -0,0 +1,26 @@
+//Guardem el video a la constant video
+const video = document.getElementById("mi-video");
+//Guardem els botons a constants
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+
+
+//Haz que se ejecute al comienzo de todo
+video.play();
+
+
+//Quan fas click al boto fa play al video
+playButton.addEventListener('click', () => {
+    if (video.paused){
+        video.play();
+    }
+    else{
+        //alerta si el video se esta reproduint ya salta.
+        window.alert("El video se esta reproduint ya");
+    }
+  });
+
+//Quan fas click al boto fa pause al video
+pauseButton.addEventListener('click', () => {
+    video.pause();
+    });
